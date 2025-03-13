1788ccea92e03df62e157c6b878ded81b9d936a3
Dante4rt <rxmxdhxni@gmail.com>
2025-03-13 14:16:19 +0700

    fix: Enhance governance and staking modules with proposal management and balance operations

--- src/governance.rs
+++ src/governance.rs
@@ -2,16 +2,17 @@ use crate::staking::StakingConfig;
 use crate::system::SystemConfig;
 use std::collections::HashMap;
 
-pub trait GovernanceConfig: StakingConfig {}
+pub trait GovernanceConfig: StakingConfig + SystemConfig {}
 
-pub struct Proposal {
+pub struct Proposal<T: GovernanceConfig> {
     description: String,
     yes_votes: u32,
     no_votes: u32,
     status: ProposalStatus,
+    creator: T::AccountId,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub enum ProposalStatus {
     Active,
     Approved,
@@ -19,14 +20,18 @@ pub enum ProposalStatus {
 }
 
 pub struct GovernancePallet<T: GovernanceConfig> {
-    pub proposals: HashMap<u32, Proposal>,
+    pub proposals: HashMap<u32, Proposal<T>>,
     pub votes: HashMap<(T::AccountId, u32), bool>, // (voter, proposal_id) -> vote_type
     next_proposal_id: u32,
 }
 
 impl<T: GovernanceConfig> GovernancePallet<T> {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            proposals: HashMap::new(),
+            votes: HashMap::new(),
+            next_proposal_id: 0,
+        }
     }
 
     // Create a new proposal
@@ -35,7 +40,20 @@ impl<T: GovernanceConfig> GovernancePallet<T> {
         creator: T::AccountId,
         description: String,
     ) -> Result<u32, &'static str> {
-        todo!()
+        let proposal_id = self.next_proposal_id;
+        self.next_proposal_id += 1;
+
+        let proposal = Proposal {
+            description,
+            yes_votes: 0,
+            no_votes: 0,
+            status: ProposalStatus::Active,
+            creator,
+        };
+
+        self.proposals.insert(proposal_id, proposal);
+
+        Ok(proposal_id)
     }
 
     // Vote on a proposal (true = yes, false = no)
@@ -45,17 +63,62 @@ impl<T: GovernanceConfig> GovernancePallet<T> {
         proposal_id: u32,
         vote_type: bool,
     ) -> Result<(), &'static str> {
-        todo!()
+        if let Some(proposal) = self.proposals.get_mut(&proposal_id) {
+            if proposal.status != ProposalStatus::Active {
+                return Err("Proposal is not active");
+            }
+
+            if self.votes.contains_key(&(voter.clone(), proposal_id)) {
+                return Err("Voter has already voted");
+            }
+
+            self.votes.insert((voter, proposal_id), vote_type);
+
+            if vote_type {
+                proposal.yes_votes += 1;
+            } else {
+                proposal.no_votes += 1;
+            }
+
+            Ok(())
+        } else {
+            Err("Proposal does not exist")
+        }
     }
 
     // Get proposal details
-    pub fn get_proposal(&self, proposal_id: u32) -> Option<&Proposal> {
-        todo!()
+    pub fn get_proposal(&self, proposal_id: u32) -> Option<&Proposal<T>> {
+        self.proposals.get(&proposal_id)
     }
 
     // Finalize a proposal (changes status based on votes)
     pub fn finalize_proposal(&mut self, proposal_id: u32) -> Result<ProposalStatus, &'static str> {
-        todo!()
+        if let Some(proposal) = self.proposals.get_mut(&proposal_id) {
+            if proposal.status != ProposalStatus::Active {
+                return Err("Proposal is not active");
+            }
+
+            if proposal.yes_votes > proposal.no_votes {
+                proposal.status = ProposalStatus::Approved;
+            } else {
+                proposal.status = ProposalStatus::Rejected;
+            }
+
+            Ok(proposal.status.clone())
+        } else {
+            Err("Proposal does not exist")
+        }
+    }
+
+    pub fn get_proposal_details(
+        &self,
+        proposal_id: u32,
+    ) -> Result<(String, T::AccountId), &'static str> {
+        if let Some(proposal) = self.proposals.get(&proposal_id) {
+            Ok((proposal.description.clone(), proposal.creator.clone()))
+        } else {
+            Err("Proposal does not exist")
+        }
     }
 }
 
@@ -76,6 +139,10 @@ mod tests {
         let proposal_id = governance
             .create_proposal(alice, "Increase validator rewards".to_string())
             .unwrap();
+
+        let (description, creator) = governance.get_proposal_details(proposal_id).unwrap();
+        assert_eq!(description, "Increase validator rewards");
+        assert_eq!(creator, alice);
 
         // Cast votes
         governance.vote(alice, proposal_id, true).unwrap(); // Yes vote
